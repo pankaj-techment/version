f17f25ba8bda5946d9f16dd2dc8add40595dff20
pankaj-techment <121149684+pankaj-techment@users.noreply.github.com>
2023-02-26 22:33:28 +0530

    version

--- my-app/src/generate-build-version.js
+++ my-app/src/generate-build-version.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const packageJson = require('../../../package.json');
+const fs = require("fs");
+const packageJson = require("../../../package.json");
 const appVersion = packageJson.version;
 const jsonData = {
-    version: appVersion,
+  version: appVersion,
 };
 var jsonContent = JSON.stringify(jsonData);
-console.log('APP VERSION HERE: ', appVersion);
-fs.writeFile('./public/meta.json', jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log('An error occured while writing JSON Object to meta.json');
-        return console.log(err);
-    }
-    console.log('meta.json file has been saved with latest version number');
+console.log("APP VERSION HERE: ", appVersion);
+fs.writeFile("./public/meta.json", jsonContent, "utf8", function (err) {
+  if (err) {
+    console.log("An error occured while writing JSON Object to meta.json");
+    return console.log(err);
+  }
+  console.log("meta.json file has been saved with latest version number");
 });
