cf2c919df7b73fb732f5ac2e581f3cc09b31bc45
pankaj-techment <121149684+pankaj-techment@users.noreply.github.com>
2023-02-26 22:37:55 +0530

    Update version number

--- my-app/src/generate-build-version.js
+++ my-app/src/generate-build-version.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const packageJson = require("../../../package.json");
+const packageJson = require("../package.json");
 const appVersion = packageJson.version;
 const jsonData = {
   version: appVersion,
